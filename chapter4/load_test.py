19f52e0cd3a3205a3e5f95a68fbb71405b0ea7db
suhwan Kim <suhwan156@gmail.com>
2022-02-18 00:03:37 +0900

    python devops linux util

--- chapter4/load_test.py
+++ chapter4/load_test.py
@@ -2,5 +2,5 @@ import molotov
 
 @molotov.scenario(100)
 async def scenario_one(session):
-    async with session.get("http://localhost:5000") as grep
+    async with session.get("http://localhost:5000") as resp:
         assert resp.status == 200
